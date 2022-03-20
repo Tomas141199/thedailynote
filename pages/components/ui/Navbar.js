b06ae0ab51978eb3d9cc7079c3ee37ece86e6c47
Tomas141199 <59587765+Tomas141199@users.noreply.github.com>
2022-03-19 19:02:58 -0600

    logo arreglado

--- pages/components/ui/Navbar.js
+++ pages/components/ui/Navbar.js
@@ -1,32 +1,25 @@
-
 const Navbar = () => {
   return (
     <div className="absolute top-0 right-0 w-full">
-        <nav className="flex items-center justify-between flex-wrap p-4 bg-slate-500 opacity-80">
-            <div className="flex items-center text-white mr-6">
-                <img src="../img/logo-tdn.svg"  alt="logo tdn"
-                    width="40"
-                />
-        
-            </div>
-            <div className=" block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                    hola este es el fondo del boton
-                </button>
-            </div>
-            <div className="text-white text-2xl">
-                The Daily Note
-            </div>
-            <div className="flex items-center justify-around flex-wrap text-white ">
-                <a className="pr-4">¿Quines somos?</a>
-                <a className="pr-4">Contacto</a>
-                <a className="pr-4">Acerca de</a>
-                <a className="pr-4">Usuario</a>
-            </div>
-        </nav>
-        
+      <nav className="flex items-center justify-between flex-wrap p-4 bg-slate-500 opacity-80">
+        <div className="flex items-center text-white mr-6">
+          <img src="../img/logo-tdn.svg" alt="logo tdn" width="70" />
+        </div>
+        <div className=" block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            hola este es el fondo del boton
+          </button>
+        </div>
+        <div className="text-white text-2xl">The Daily Note</div>
+        <div className="flex items-center justify-around flex-wrap text-white ">
+          <a className="pr-4">¿Quines somos?</a>
+          <a className="pr-4">Contacto</a>
+          <a className="pr-4">Acerca de</a>
+          <a className="pr-4">Usuario</a>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,15 @@ import Navbar from "./components/ui/Navbar";
 export default function Home() {
   return (
     <div>
-      <img src="/img/hero.jpg" className="w-screen max-h-96 relative object-cover object-top"/>
-      <Navbar/>
+      <img
+        src="/img/hero.jpg"
+        className="w-screen max-h-96 relative object-cover object-top"
+      />
+      <Navbar />
 
       <div className="mt-6 text-2xl text-black text-center animate-bounce 1s infinity">
         Hola
       </div>
-      
     </div>
   );
 }
