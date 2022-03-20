7bc8b1bbb1326f82725ba11c4bcd76d7b7cdf70f
Juan Santos <jukrolso@gmail.com>
2022-03-20 09:44:46 -0600

    Interfaz registro

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import styled from '@emotion/styled'
+import Registro from './registro'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( //<Component {...pageProps} />
+
+    <Registro></Registro>
+
+  )
 }
 
 export default MyApp
