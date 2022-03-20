38ee0a0b9eaf319fa87e097570cbb4d48904001f
Luis Donaldo Galloso <luisdonaldo.gt@gmail.com>
2022-03-19 19:51:55 -0600

    actualización de navbar 2.0
    solo faltaría un drop down

--- pages/components/ui/Navbar.js
+++ pages/components/ui/Navbar.js
@@ -1,22 +1,35 @@
 const Navbar = () => {
   return (
     <div className="absolute top-0 right-0 w-full">
-      <nav className="flex items-center justify-between flex-wrap p-4 bg-slate-500 opacity-80">
-        <div className="flex items-center text-white mr-6">
-          <img src="../img/logo-tdn.svg" alt="logo tdn" width="70" />
+      <nav className=" text-center md:flex md:items-center md:justify-between md:flex-wrap py-4 bg-slate-800 opacity-80">
+        <div className=" md:flex md:items-center">
+          <a href="/">
+            <img className="mx-auto md:ml-3" src="../img/logo-tdn.svg" alt="logo tdn" width="40" />
+          </a>
+          
         </div>
-        <div className=" block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <div className="hidden md:block text-white text-2xl">
+          <p>The Daily Note</p>
+        </div>
+        <ul className=" md:flex md:items-center md:justify-around text-white mt-6 md:mt-0">
+          <li className=" my-3 md:my-0">
+            <a href="#" className=" pr-4 hover:text-amber-300 ">¿Quines somos?</a>
+          </li>
+          <li className=" my-3 md:my-0">
+            <a href="#" className="pr-4 hover:text-amber-300">Contacto</a>
+          </li>
+          <li className=" my-3 md:my-0">
+            <a href="#" className="pr-4 hover:text-amber-300">Acerca de</a>
+          </li>
+          <li className=" my-3 md:my-0">
+            <a href="#" className="pr-4 hover:text-amber-300">Usuario</a>
+          </li>
+        </ul>
+        {/* <div className="block lg:hidden">
+          <button className=" flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
             hola este es el fondo del boton
           </button>
-        </div>
-        <div className="text-white text-2xl">The Daily Note</div>
-        <div className="flex items-center justify-around flex-wrap text-white ">
-          <a className="pr-4">¿Quines somos?</a>
-          <a className="pr-4">Contacto</a>
-          <a className="pr-4">Acerca de</a>
-          <a className="pr-4">Usuario</a>
-        </div>
+        </div> */}
       </nav>
     </div>
   );
